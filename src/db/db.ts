53b63d95cb33a73e4a7e99a659962f93b34b6a32
devesh chandra <deveshru2712@gmail.com>
2026-02-26 16:21:58 +0530

    fix: switch from http driver to serverless

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,11 +1,13 @@
-import { neon } from "@neondatabase/serverless";
-import "dotenv/config";
-import { drizzle } from "drizzle-orm/neon-http";
+import { drizzle } from "drizzle-orm/neon-serverless";
+import ws from "ws";
 
 import { env } from "../config/env";
 import { schema } from "./schema";
 
-const sql = neon(env.DATABASE_URL!);
-const db = drizzle({ client: sql, schema });
+const db = drizzle({
+  connection: env.DATABASE_URL,
+  ws: ws,
+  schema,
+});
 
 export default db;
